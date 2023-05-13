a3426eba63313a7d94dedd506ad47c782141d806
Nguyen Hai Duc <nguyenhaiduc06@gmail.com>
2023-05-13 10:52:47 +0700

    update: set theme base on color scheme

--- App.js
+++ App.js
@@ -3,9 +3,24 @@ import { NavigationContainer } from "@react-navigation/native";
 import { MainNavigator } from "./src/navigators";
 import { ThemeProvider } from "styled-components/native";
 import { darkTheme, lightTheme } from "./src/theme";
+import { useEffect, useState } from "react";
+import { Appearance } from "react-native";
+import { Theme } from "./src/theme";
 
 export default function App() {
-  const theme = lightTheme;
+  const [theme, setTheme] = useState(lightTheme);
+
+  useEffect(() => {
+    const removeListener = Appearance.addChangeListener((preferences) => {
+      const colorScheme = preferences.colorScheme;
+      const theme = colorScheme == "light" ? lightTheme : darkTheme;
+      setTheme(theme);
+    });
+    return () => {
+      removeListener();
+    };
+  }, []);
+
   return (
     <ThemeProvider theme={theme}>
       <NavigationContainer theme={theme}>
