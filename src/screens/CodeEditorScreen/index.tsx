be842f97731e02076bf1464b92cf4846b3c042e2
Nguyen Hai Duc <nguyenhaiduc06@gmail.com>
2023-05-14 22:07:36 +0700

    remove: syntax highlight in code editor

--- src/screens/CodeEditorScreen/index.tsx
+++ src/screens/CodeEditorScreen/index.tsx
@@ -1,26 +1,27 @@
 import { RouteProp, useRoute } from "@react-navigation/native";
 import React, { useEffect, useState } from "react";
-import { Keyboard, ScrollView, View } from "react-native";
+import { Keyboard, ScrollView, TextInput, View } from "react-native";
 import { styled } from "styled-components/native";
 import { MainStackParamList } from "../../navigators";
 import { leetCode } from "../../core/LeetCode";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
-import CodeEditor, {
-  CodeEditorSyntaxStyles,
-} from "@rivascva/react-native-code-editor";
 import Animated, {
   useAnimatedKeyboard,
   useAnimatedStyle,
 } from "react-native-reanimated";
-import { Button, Icon } from "../../components";
+import { Button, Icon, Text } from "../../components";
 import { useTheme } from "../../hooks";
 import { Header } from "./Header";
 import { Console } from "./Console";
 import { CodeInterpretResult, CodeEditorData } from "../../core/types";
 
+const Row = styled.View`
+  flex-direction: row;
+  align-items: center;
+  z-index: 2;
+`;
+
 const ToolbarContainer = styled.View`
-  height: 40px;
-  border-radius: 6px;
   flex-direction: row;
   align-items: center;
 `;
@@ -32,10 +33,9 @@ const ToolbarButton = styled.TouchableOpacity`
   height: 40px;
 `;
 
-const ToolbarText = styled.Text`
-  font-size: 16px;
-  font-family: "Menlo";
-  color: ${(p) => p.theme.colors.text};
+const MonoText = styled(Text)`
+  font-size: 17px;
+  font-family: "IBMPlexMono_400Regular";
 `;
 
 export const CodeEditorScreen = () => {
@@ -43,7 +43,6 @@ export const CodeEditorScreen = () => {
   const [codeInterpretResult, setCodeInterpretResult] =
     useState<CodeInterpretResult>(null);
   const [codeInterpretPending, setCodeInterpretPending] = useState(false);
-  console.log("🚀 ~ codeInterpretResult:", codeInterpretResult);
   const [codeSubmitResult, setCodeSubmitResult] = useState(null);
 
   const [typedCode, setTypedCode] = useState("");
@@ -57,18 +56,15 @@ export const CodeEditorScreen = () => {
   const { titleSlug } = route.params;
   const translatedStyle = useAnimatedStyle(() => {
     return {
-      width: "100%",
-      paddingVertical: 8,
-      paddingRight: 8,
-      backgroundColor: theme.colors.background,
-      flexDirection: "row",
-      alignItems: "center",
       position: "absolute",
       bottom:
         keyboard.height.value > insets.bottom
           ? keyboard.height.value
           : insets.bottom,
       left: 0,
+      width: "100%",
+      paddingVertical: 8,
+      backgroundColor: theme.colors.background,
     };
   });
 
@@ -122,78 +118,81 @@ export const CodeEditorScreen = () => {
     setShowConsole((show) => !show);
   };
 
-  if (!initialCode) {
-    return <View />;
-  }
-
   return (
     <View style={{ flex: 1 }}>
       <Header onSubmit={submit} onRun={run} />
-      <CodeEditor
-        initialValue={initialCode}
-        autoFocus={false}
+      <TextInput
+        defaultValue={initialCode}
         style={{
-          fontSize: 19,
-          inputLineHeight: 26,
-          highlighterLineHeight: 26,
-          backgroundColor: "transparent",
+          flex: 1,
+          color: theme.colors.text,
+          paddingHorizontal: 16,
+          fontSize: 16,
           fontFamily: "IBMPlexMono_400Regular",
         }}
-        language="python"
-        syntaxStyle={CodeEditorSyntaxStyles.atomOneDark}
-        showLineNumbers
-        onChange={setTypedCode}
+        multiline
+        onChangeText={setTypedCode}
+        textAlignVertical="top"
+        onScroll={() => console.log("scrolling")}
       />
       <Animated.View style={translatedStyle}>
-        <ScrollView
-          horizontal
-          keyboardDismissMode="none"
-          keyboardShouldPersistTaps="always"
-        >
-          <ToolbarContainer>
-            <ToolbarButton>
-              <Icon
-                name="indent-increase"
-                size={16}
-                color={theme.colors.text}
-              />
-            </ToolbarButton>
-            <ToolbarButton>
-              <Icon
-                name="indent-decrease"
-                size={16}
-                color={theme.colors.text}
-              />
-            </ToolbarButton>
-            {["(", "[", "{", ":", ";", "'", '"'].map((char) => (
-              <ToolbarButton
-                key={char}
-                onLongPress={() => {
-                  console.log("opening submenu");
-                }}
-              >
-                <ToolbarText>{char}</ToolbarText>
-              </ToolbarButton>
-            ))}
-          </ToolbarContainer>
-        </ScrollView>
-        <View
-          style={{
-            borderRightWidth: 1,
-            borderRightColor: theme.colors.border,
-            height: 20,
-          }}
-        />
-        <ToolbarButton onPress={dismiss}>
-          <Icon name="keyboard-box-line" size={16} color={theme.colors.text} />
-        </ToolbarButton>
         <Button
           iconName="terminal-box-line"
           label="Console"
           size="sm"
           onPress={toggleConsole}
+          style={{ alignSelf: "flex-start", margin: 8 }}
         />
+        <Row>
+          <ScrollView
+            horizontal
+            keyboardDismissMode="none"
+            keyboardShouldPersistTaps="always"
+          >
+            <ToolbarContainer>
+              <ToolbarButton>
+                <Icon
+                  name="indent-decrease"
+                  size={16}
+                  color={theme.colors.text}
+                />
+              </ToolbarButton>
+              <ToolbarButton>
+                <Icon
+                  name="indent-increase"
+                  size={16}
+                  color={theme.colors.text}
+                />
+              </ToolbarButton>
+              {["(", "[", "{", ":", ";", "'", '"'].map((char) => (
+                <ToolbarButton
+                  key={char}
+                  onLongPress={() => {
+                    console.log("opening submenu");
+                  }}
+                >
+                  <MonoText>{char}</MonoText>
+                </ToolbarButton>
+              ))}
+            </ToolbarContainer>
+          </ScrollView>
+          <View
+            style={{
+              borderRightWidth: 1,
+              borderRightColor: theme.colors.border,
+              height: 24,
+            }}
+          />
+          <ToolbarButton onPress={dismiss}>
+            <Icon
+              name="keyboard-box-line"
+              size={16}
+              color={theme.colors.text}
+            />
+          </ToolbarButton>
+        </Row>
       </Animated.View>
+
       <Console
         visible={showConsole}
         dismiss={() => setShowConsole(false)}
