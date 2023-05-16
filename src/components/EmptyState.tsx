9d28d8129ccf9c52915b443fcf09d181fa6d1f4b
Nguyen Hai Duc <nguyenhaiduc06@gmail.com>
2023-05-16 11:34:22 +0700

    add: empty state component

---
+++ src/components/EmptyState.tsx
@@ -0,0 +1,27 @@
+import React, { FC } from "react";
+import { styled } from "styled-components/native";
+import { Icon, Text } from ".";
+import { useTheme } from "../hooks";
+import { ViewProps } from "react-native";
+
+type EmptyStateProps = ViewProps & {
+  label: string;
+  iconName: string;
+};
+
+export const EmptyState: FC<EmptyStateProps> = (props) => {
+  const theme = useTheme();
+  const { label, iconName, ...rest } = props;
+  return (
+    <Container {...rest}>
+      <Icon name={iconName} size={32} color={theme.colors.textDim} />
+      <Text dim>{label}</Text>
+    </Container>
+  );
+};
+
+const Container = styled.View`
+  align-items: center;
+  justify-content: center;
+  gap: 8px;
+`;
