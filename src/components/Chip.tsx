618e6a44a42ccf652c96d87a04bf798663629352
Nguyen Hai Duc <nguyenhaiduc06@gmail.com>
2023-05-17 08:27:07 +0700

    update: chip component

--- src/components/Chip.tsx
+++ src/components/Chip.tsx
@@ -1,14 +1,16 @@
 import { FC } from "react";
 import { ViewProps } from "react-native";
 import { styled } from "styled-components/native";
+import { useTheme } from "../hooks";
 
 type ChipProps = ViewProps & {
   backgroundColor?: string;
   label: string;
   labelColor?: string;
+  borderColor?: string;
 };
 
-const Container = styled.View<{ backgroundColor?: string }>`
+const Container = styled.View<{ backgroundColor: string; borderColor: string }>`
   background-color: ${({ backgroundColor, theme }) =>
     backgroundColor ?? theme.colors.foreground};
   border-radius: 50%;
@@ -16,6 +18,7 @@ const Container = styled.View<{ backgroundColor?: string }>`
   padding: 0px 8px;
   align-items: center;
   justify-content: center;
+  border: 1px solid ${(p) => p.borderColor};
 `;
 
 const Label = styled.Text<{ color?: string }>`
@@ -23,9 +26,21 @@ const Label = styled.Text<{ color?: string }>`
   color: ${({ color, theme }) => color ?? theme.colors.text};
 `;
 
-export const Chip: FC<ChipProps> = ({ label, labelColor, ...rest }) => {
+export const Chip: FC<ChipProps> = (props) => {
+  const theme = useTheme();
+  const {
+    label,
+    labelColor = theme.colors.text,
+    backgroundColor = theme.colors.foreground,
+    borderColor = theme.colors.foreground,
+    ...rest
+  } = props;
   return (
-    <Container {...rest}>
+    <Container
+      backgroundColor={backgroundColor}
+      borderColor={borderColor}
+      {...rest}
+    >
       <Label color={labelColor}>{label}</Label>
     </Container>
   );
