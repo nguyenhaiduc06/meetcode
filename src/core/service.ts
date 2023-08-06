5c5f9bc7911ded10d3b25fee815af3f35abc049d
Nguyen Hai Duc <nguyenhaiduc06@gmail.com>
2023-08-06 18:01:24 +0700

    UPDATE: cookie

--- src/core/service.ts
+++ src/core/service.ts
@@ -13,10 +13,12 @@ var _httpClient: AxiosInstance;
 class Service {
   session: string;
   csrfToken: string;
+  cookie: string;
 
   constructor(session, csrfToken) {
     this.session = session;
     this.csrfToken = csrfToken;
+    this.cookie = `__stripe_mid=4961e205-7bac-4fd7-95de-21bf4fad2ad97161fb; csrftoken=Rm92dEBADq5QtsBGPHufMMkoU1OtQSwen7WKk2mUpFMrKiVUf1720fd4NevOTCIX; LEETCODE_SESSION=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJfYXV0aF91c2VyX2lkIjoiNDkwNTE4NyIsIl9hdXRoX3VzZXJfYmFja2VuZCI6ImFsbGF1dGguYWNjb3VudC5hdXRoX2JhY2tlbmRzLkF1dGhlbnRpY2F0aW9uQmFja2VuZCIsIl9hdXRoX3VzZXJfaGFzaCI6IjQxYzQ1MTFlMWM4NjI0ZDg1NTJhNWI2ZDlmMjdjOGFlN2NmMTkwNTEiLCJpZCI6NDkwNTE4NywiZW1haWwiOiJuZ3V5ZW5oYWlkdWMwNkBnbWFpbC5jb20iLCJ1c2VybmFtZSI6ImhhaWR1YzA2IiwidXNlcl9zbHVnIjoiaGFpZHVjMDYiLCJhdmF0YXIiOiJodHRwczovL3MzLXVzLXdlc3QtMS5hbWF6b25hd3MuY29tL3MzLWxjLXVwbG9hZC9hc3NldHMvZGVmYXVsdF9hdmF0YXIuanBnIiwicmVmcmVzaGVkX2F0IjoxNjkxMTI3MDEwLCJpcCI6IjExMy4xNjguMjUxLjEzNSIsImlkZW50aXR5IjoiM2I4ZDM5OWI1NmZiOWRmNTU5MmIwNTFmZGUzNmM5MDMiLCJzZXNzaW9uX2lkIjo0MzcxNDExMiwiX3Nlc3Npb25fZXhwaXJ5IjoxMjA5NjAwfQ.aXmTS9NydbdEy3_VGg1dhd-JBu-S2KBp1YkNsvZDGSM; NEW_PROBLEMLIST_PAGE=1; _dd_s=rum=0&expire=1691129601904`;
   }
 
   GraphQLQuery(options: QueryOptions) {
@@ -26,7 +28,7 @@ class Service {
         headers: {
           Origin: endpoint.base,
           Referer: endpoint.base,
-          Cookie: `__stripe_mid=7ceabc7d-c82a-40bf-8533-efcb88d60989cd6aac; csrftoken=2IvoH9mr4rvC4Idy4gKdHdd8sB8xkOEDYKnoEg3pgs91VbQjoPvhMNGiYdNgos8D; LEETCODE_SESSION=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJfYXV0aF91c2VyX2lkIjoiNDkwNTE4NyIsIl9hdXRoX3VzZXJfYmFja2VuZCI6ImFsbGF1dGguYWNjb3VudC5hdXRoX2JhY2tlbmRzLkF1dGhlbnRpY2F0aW9uQmFja2VuZCIsIl9hdXRoX3VzZXJfaGFzaCI6IjQxYzQ1MTFlMWM4NjI0ZDg1NTJhNWI2ZDlmMjdjOGFlN2NmMTkwNTEiLCJpZCI6NDkwNTE4NywiZW1haWwiOiJuZ3V5ZW5oYWlkdWMwNkBnbWFpbC5jb20iLCJ1c2VybmFtZSI6ImhhaWR1YzA2IiwidXNlcl9zbHVnIjoiaGFpZHVjMDYiLCJhdmF0YXIiOiJodHRwczovL3MzLXVzLXdlc3QtMS5hbWF6b25hd3MuY29tL3MzLWxjLXVwbG9hZC9hc3NldHMvZGVmYXVsdF9hdmF0YXIuanBnIiwicmVmcmVzaGVkX2F0IjoxNjgzODYzNzkxLCJpcCI6IjE0LjE4OS4xMjIuMjI0IiwiaWRlbnRpdHkiOiJmOTBmY2NiYTk5NTY1NzgwYjEwMGZiOGU5ZTE0MDZjZSIsInNlc3Npb25faWQiOjM5MzMyNzU0LCJfc2Vzc2lvbl9leHBpcnkiOjEyMDk2MDB9.HGIB119oOIi1YMdEBcwsErxBXYZEsg1mRM0avScbPUc; NEW_PROBLEMLIST_PAGE=1; _dd_s=rum=0&expire=1684032001838`,
+          Cookie: this.cookie,
           "X-Requested-With": "XMLHttpRequest",
           "X-CSRFToken": this.csrfToken,
         },
@@ -42,7 +44,7 @@ class Service {
       _httpClient = axios.create({
         baseURL: endpoint.base,
         headers: {
-          Cookie: `__stripe_mid=7ceabc7d-c82a-40bf-8533-efcb88d60989cd6aac; csrftoken=2IvoH9mr4rvC4Idy4gKdHdd8sB8xkOEDYKnoEg3pgs91VbQjoPvhMNGiYdNgos8D; NEW_PROBLEMLIST_PAGE=1; LEETCODE_SESSION=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJfYXV0aF91c2VyX2lkIjoiNDkwNTE4NyIsIl9hdXRoX3VzZXJfYmFja2VuZCI6ImFsbGF1dGguYWNjb3VudC5hdXRoX2JhY2tlbmRzLkF1dGhlbnRpY2F0aW9uQmFja2VuZCIsIl9hdXRoX3VzZXJfaGFzaCI6IjQxYzQ1MTFlMWM4NjI0ZDg1NTJhNWI2ZDlmMjdjOGFlN2NmMTkwNTEiLCJpZCI6NDkwNTE4NywiZW1haWwiOiJuZ3V5ZW5oYWlkdWMwNkBnbWFpbC5jb20iLCJ1c2VybmFtZSI6ImhhaWR1YzA2IiwidXNlcl9zbHVnIjoiaGFpZHVjMDYiLCJhdmF0YXIiOiJodHRwczovL3MzLXVzLXdlc3QtMS5hbWF6b25hd3MuY29tL3MzLWxjLXVwbG9hZC9hc3NldHMvZGVmYXVsdF9hdmF0YXIuanBnIiwicmVmcmVzaGVkX2F0IjoxNjgzODYzNzkxLCJpcCI6IjE0LjE4OS4xMjIuMjI0IiwiaWRlbnRpdHkiOiJmOTBmY2NiYTk5NTY1NzgwYjEwMGZiOGU5ZTE0MDZjZSIsInNlc3Npb25faWQiOjM5MzMyNzU0LCJfc2Vzc2lvbl9leHBpcnkiOjEyMDk2MDB9.HGIB119oOIi1YMdEBcwsErxBXYZEsg1mRM0avScbPUc; _dd_s=rum=0&expire=1683908640897`,
+          Cookie: this.cookie,
           "X-Requested-With": "XMLHttpRequest",
           "X-CSRFToken": this.csrfToken,
           Referer: endpoint.base,
